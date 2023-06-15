3ced857b96a2b4574e011eff35d486551bcb3a97
szykor18 <115345580+szykor18@users.noreply.github.com>
2023-06-15 22:03:15 +0200

    Update FileManager.java

--- FileManager/src/FileManager.java
+++ FileManager/src/FileManager.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.util.Comparator;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -25,7 +24,6 @@ public class FileManager {
         // you can sort files in many ways, I sort based on file's weight
         Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
                 .filter(File::isFile)
-
                 .sorted((x1, x2) -> Long.compare(x2.length(), x1.length()))
                 .forEach(file -> {
                     System.out.println(file.getName());
@@ -46,7 +44,6 @@ public class FileManager {
     public void listSortedEverything(String directory) {
         // you can sort files in many ways, I sort based on file's weight
         Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
-
                 .sorted((x1, x2) -> Long.compare(x2.length(), x1.length()))
                 .forEach(file -> {
                     System.out.println(file.getName());
